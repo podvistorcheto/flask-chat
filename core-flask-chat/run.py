f4fc2cbf3566a22fb6e2af1ca89401bd20a5d757
podvistorcheto <denev.s.deyan@gmail.com>
2020-03-29 13:22:23 +0000

    added ability to write and display messages for the user

--- core-flask-chat/run.py
+++ core-flask-chat/run.py
@@ -1,15 +1,31 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    """ add messages to messages list"""
+    messages.append("{}: {}".format(username, message))
+
+def get_all_messages():
+    """Get all the messages and separete the with </br> tag"""
+    return "<br>".join(messages)
 
 @app.route("/")
 def index():
     """Main page with instructions"""
     return "to send a message use /USERNAME/MESSAGE"
 
-@app.route('<username>')
+@app.route('/<username>')
 def user(username):
-    return "Hi "+ username
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username,message)
+    return redirect("/" + username)
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')),debug=True)
