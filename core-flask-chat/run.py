5f57e4b1dace197b2f517114e0c2251cebc6a826
podvistorcheto <denev.s.deyan@gmail.com>
2020-03-29 12:15:10 +0000

    Implement basic views and routes

--- core-flask-chat/run.py
+++ core-flask-chat/run.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello There!</h1>"
+    """Main page with instructions"""
+    return "to send a message use /USERNAME/MESSAGE"
+
+@app.route('<username>')
+def user(username):
+    return "Hi "+ username
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')),debug=True)
