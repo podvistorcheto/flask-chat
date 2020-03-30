bc47daa355e0d7671c57ffbf582084b1b0900ffb
podvistorcheto <denev.s.deyan@gmail.com>
2020-03-30 06:22:40 +0000

    cookie session added

--- core-flask-chat/run.py
+++ core-flask-chat/run.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
@@ -14,10 +15,17 @@ def get_all_messages():
     """Get all the messages and separete the with </br> tag"""
     return "<br>".join(messages)
 
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def index():
     """Main page with instructions"""
-    return "to send a message use /USERNAME/MESSAGE"
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
 
 @app.route('/<username>')
 def user(username):
