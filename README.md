f90a268dc25dab0ce184f6d126a8a8a7bca5f068
podvistorcheto <denev.s.deyan@gmail.com>
2020-04-02 07:40:43 +0000

    New commit to deploy to Heroku

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
+# Flask Chat app
+
+This is a chat application written in Flask. The purpose of this application is to 
+take data from a URL and store the information  in a list.
+
+
+
 <img src="https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png" style="margin: 0;">
 
-Welcome USER_NAME,
+Welcome podvistorcheto,
 
 This is the Code Institute student template for Gitpod. We have preinstalled all of the tools you need to get started. You can safely delete this README.md file, or change it for your own project.
 
---
+++ run.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "randomstring123")
+messages = []   
+
+
+def add_message(username, message):
+    """ add messages to 'messages' list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username,"message": message})
+
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"]=request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
+
+
+@app.route("/chat/<username>", methods=["GET","POST"])
+def user(username):
+    """Add and display chat messages"""    
+    if request.method == "POST":
+        username=session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        """ if comment out line 41 the message typed will come again and repeart forever"""
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username=username, chat_messages=messages)
+
+
+app.run(host=os.getenv('IP', "0.0.0.0"), port=int(os.getenv('PORT', "5000")), debug=False)
